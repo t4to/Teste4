49fcdbe34e94ca91d94b55f0dd8c0eba12b3321c
LRC <tato.rodrigues@gmail.com>
2011-10-26 16:37:30 -0200

    Inflection

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -21,19 +21,13 @@ ActiveSupport::Inflector.inflections do |inflect|
   inflect.plural(/ul$/i,  'uis')
   inflect.plural(/([^aeou])il$/i,  '\1is')
   inflect.plural(/m$/i,   'ns')
-  inflect.plural(/^(japon|escoc|ingl|dinamarqu|fregu|portugu)ês$/i,  '\1eses')
-  inflect.plural(/^(|g)ás$/i,  '\1ases')
-  inflect.plural(/ão$/i,  'ões')
-  inflect.plural(/^(irm|m)ão$/i,  '\1ãos')
-  inflect.plural(/^(alem|c|p)ão$/i,  '\1ães')
+
 
   # Sem acentos...
   inflect.plural(/ao$/i,  'oes')
   inflect.plural(/^(irm|m)ao$/i,  '\1aos')
   inflect.plural(/^(alem|c|p)ao$/i,  '\1aes')
 
-  inflect.singular(/([^ê])s$/i, '\1')
-  inflect.singular(/^(á|gá|paí)s$/i, '\1s')
   inflect.singular(/(r|z)es$/i, '\1')
   inflect.singular(/([^p])ais$/i, '\1al')
   inflect.singular(/eis$/i, 'el')
@@ -42,18 +36,15 @@ ActiveSupport::Inflector.inflections do |inflect|
   inflect.singular(/(r|t|f|v)is$/i, '\1il')
   inflect.singular(/sses$/i, 'sse')
   inflect.singular(/^(.*[^s]s)es$/i, '\1')
-  inflect.singular(/ães$/i, 'ão')
-  inflect.singular(/aes$/i, 'ao')
-  inflect.singular(/ãos$/i, 'ão')    
+  inflect.singular(/aes$/i, 'ao')    
   inflect.singular(/aos$/i, 'ao')
-  inflect.singular(/ões$/i, 'ão')
   inflect.singular(/oes$/i, 'ao')
   inflect.singular(/(japon|escoc|ingl|dinamarqu|fregu|portugu)eses$/i, '\1ês')
   inflect.singular(/^(g|)ases$/i,  '\1ás')
 
   # Incontáveis
-  inflect.uncountable %w( tórax tênis ônibus lápis fênix )
+  #inflect.uncountable %w( tórax tênis ônibus lápis fênix )
 
   # Irregulares
-  inflect.irregular "país", "países"
+  #inflect.irregular "país", "países"
 end
