66e7c7d3b94ae1b37793a411a6695489a9225823
LRC <tato.rodrigues@gmail.com>
2011-10-26 16:46:12 -0200

    Inflection

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,34 +10,7 @@
 # end
 
 ActiveSupport::Inflector.inflections do |inflect|
-  inflect.clear
-
-  inflect.plural(/$/,  's')
-  inflect.plural(/(s)$/i,  '\1')
-  inflect.plural(/(z|r)$/i, '\1es')
-  inflect.plural(/al$/i,  'ais')
-  inflect.plural(/el$/i,  'eis')
-  inflect.plural(/ol$/i,  'ois')
-  inflect.plural(/ul$/i,  'uis')
-  inflect.plural(/([^aeou])il$/i,  '\1is')
-  inflect.plural(/m$/i,   'ns')
-
-
-  # Sem acentos...
-  inflect.plural(/ao$/i,  'oes')
-  inflect.plural(/^(irm|m)ao$/i,  '\1aos')
-  inflect.plural(/^(alem|c|p)ao$/i,  '\1aes')
-
-  inflect.singular(/(r|z)es$/i, '\1')
-  inflect.singular(/([^p])ais$/i, '\1al')
-  inflect.singular(/eis$/i, 'el')
-  inflect.singular(/ois$/i, 'ol')
-  inflect.singular(/uis$/i, 'ul')
-  inflect.singular(/(r|t|f|v)is$/i, '\1il')
-  inflect.singular(/sses$/i, 'sse')
-  inflect.singular(/^(.*[^s]s)es$/i, '\1')
-  inflect.singular(/aes$/i, 'ao')    
-  inflect.singular(/aos$/i, 'ao')
-  inflect.singular(/oes$/i, 'ao')
-  
+	inflect.irregular 'categoria', 'categorias'
+	inflect.irregular 'perfil', 'perfis'
+	inflect.irregular 'admin', 'admins'
 end
